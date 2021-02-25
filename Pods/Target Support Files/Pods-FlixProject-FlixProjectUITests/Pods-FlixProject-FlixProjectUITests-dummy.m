16ea759f1fe2269d9092f4bdfbb6504516104d7a
Vivianne Ramselaar <vmb318@nyu.edu>
2021-02-25 15:11:41 -0500

    part 2

---
+++ Pods/Target Support Files/Pods-FlixProject-FlixProjectUITests/Pods-FlixProject-FlixProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixProject_FlixProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixProject_FlixProjectUITests
+@end
---
+++ Pods/Target Support Files/Pods-FlixProjectTests/Pods-FlixProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixProjectTests
+@end
